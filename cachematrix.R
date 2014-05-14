38faba007b5e13e0d9372c9a564df10f07b2ec38
taiwofasae <taiwofasae@yahoo.co.uk>
2014-05-15 00:40:19 +0100

    edited cacheMatrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two functions makeCacheMatrix and cacheSolve make use of lexical scoping
+## to cache matrix inverse calculations.  The cacheSolve function
+## is used in a similar way to the Solve function for
+## matrix inverse computation
 
-## Write a short comment describing this function
+
+## makeCacheMatrix creates a special "matrix" which is a list, 
+## as in the case in the question which is 
+## really a matrix containing a function to
+## set the value of the matrix
+## get the value of the matrix
+## set the value of the matrix inverse
+## get the value of the matrix inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inv) i <<- inv
+        getinv <- function() i
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## The following function calculates the matrix inverse
+## of the special "matrix" created with the above function: makeCacheMatrix
+## However, it first checks to see if the inverse has already 
+## been calculated. If so, it gets the matrix inverse
+## from the cache and skips the computation. Otherwise, 
+## it calculates the matrix inverse of the data and sets the value 
+## of the matrix inverse in the cache via the setinv function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        i <- x$getinv()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setinv(i)
+        i
 }
